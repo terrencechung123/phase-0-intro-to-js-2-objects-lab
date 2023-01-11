b15a4c70cec01097a3b634d3338f1fae3dab0b6d
Terrence Chung <terrencechung35@gmail.com>
2023-01-11 10:07:11 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Paco'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee['streetAddress'] = '11 Broadway'
+
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(Employee, key, value) {
+    const streetAddress = 'delete employee.name'
+    employee['streetAddress'] = '12 Broadway'
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {newEmployee}
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, Key) {
+    delete employee.name
+    return employee
+}
